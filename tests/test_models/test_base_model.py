ce902410d579d2bb3df34cebc4387558f50dfda5
ayele-getu <102233285+ayele-getu@users.noreply.github.com>
2022-09-05 18:06:57 +0300

    Update test_base_model.py

--- tests/test_models/test_base_model.py
+++ tests/test_models/test_base_model.py
@@ -1,9 +1,5 @@
 #!/usr/bin/python3
-# -*- coding: utf-8 -*-
-"""
-Created on Fri Jun  5 15:43:09 2020
-@author: meco
-"""
+"""Tests for the BaseModel class"""
 import sys
 import unittest
 import inspect
