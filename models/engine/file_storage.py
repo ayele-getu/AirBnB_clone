c0ed22048e047ce7b0d9321b02eaca9a6d2e05c0
ayele-getu <102233285+ayele-getu@users.noreply.github.com>
2022-09-05 14:23:01 +0300

    Update file_storage.py

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -1,44 +1,43 @@
 #!/usr/bin/python3
+"""Contain the class FileStorage"""
+
+from models.base_model import BaseModel
+from models.user import User
+from models.state import State
+from models.city import City
+from models.amenity import Amenity
+from models.review import Review
+from models.place import Place
 import json
-import os
 
 
-class FileStorage:
-    """ Class FileStorage """
+class FileStorage():
+    """ FileStorage Class"""
     __file_path = "file.json"
     __objects = {}
 
     def all(self):
-        """ Return Objects """
-        return (FileStorage.__objects)
+        """Return all objects from dictionary __objects"""
+        return self.__objects
 
     def new(self, obj):
-        """ Create New Object """
-        FileStorage.__objects["{}.{}".format(
-            obj.__class__.__name__, obj.id)] = obj
-
-    def save(self):
-        """ Save File Json """
-        with open(FileStorage.__file_path, 'w') as file_json:
-            res = {}
-            for key, value in FileStorage.__objects.items():
-                res[key] = value.to_dict()
-            file_json.write(json.dumps(res))
+        """Set in __objects with key <obj class name>.id"""
+        self.__objects["{}.{}".format(obj.__class__.__name__, obj.id)] = obj
 
     def reload(self):
-        """ Reload File Json """
-        try:    
-            with open(FileStorage.__file_path, 'r', encoding="utf-8") as file_json:
-                from models.base_model import BaseModel
-                from models.user import User
-                from models.place import Place
-                from models.state import State
-                from models.city import City
-                from models.amenity import Amenity
-                from models.review import Review
-                json_des = json.load(file_json)
-            for key, value in json_des.items():
-                value = eval(value["__class__"])(**value)
-                FileStorage.__objects[key] = value
-        except OSError:
+        """deserialization the JSON file to __objects, only JS"""
+        try:
+            with open(self.__file_path, mode='r', encoding='utf-8') as f:
+                all_objs = json.loads(f.read())
+            for obj_id, obj in all_objs.items():
+                name_class = obj_id.split(".")[0]
+                self.new(eval(name_class + "(**obj)"))
+        except:
             pass
+
+    def save(self):
+        """serializes __objects to the JSON file (path: __file_path)"""
+        with open(self.__file_path, mode='w', encoding='utf-8') as f:
+            obj_dict = {key_obj: obj.to_dict()
+                        for key_obj, obj in self.__objects.items()}
+            json.dump(obj_dict, f)
