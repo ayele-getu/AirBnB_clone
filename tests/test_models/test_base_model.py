4daf9ad3f18b1f8cb764eefc935b853917de0e98
ayele-getu <102233285+ayele-getu@users.noreply.github.com>
2022-09-05 17:42:09 +0300

    Update test_base_model.py

--- tests/test_models/test_base_model.py
+++ tests/test_models/test_base_model.py
@@ -1,44 +1,68 @@
 #!/usr/bin/python3
-
-""" import modules for set id and datetime """
-import uuid
-from datetime import datetime
+'''Tests for BaseModel class'''
 import models
+from models.base_model import BaseModel
+import os
+import unittest
 
 
-class BaseModel():
-    """ BaseModel Class """
+class TestBase_Model(unittest.TestCase):
+    '''start tests'''
 
-    def __init__(self, *args, **kwargs):
+    def test_docstring(self):
+        '''test if funcions, methods, classes
+        and modules have docstring'''
+        msj = "Módulo does not has docstring"
+        self.assertIsNotNone(models.base_model.__doc__, msj)  # Modules
+        msj = "Clase does not has docstring"
+        self.assertIsNotNone(BaseModel.__doc__, msj)  # Classes
 
-        if bool(kwargs):
-            for name_attr, value in kwargs.items():
-                if name_attr in ["created_at", "updated_at"]:
-                    setattr(self,
-                            name_attr,
-                            datetime.strptime(value, '%Y-%m-%dT%H:%M:%S.%f'))
-                elif name_attr != "__class__":
-                    setattr(self, name_attr, value)
-        else:
-            self.id = str(uuid.uuid4())
-            self.created_at = datetime.now()
-            self.updated_at = self.created_at
-            models.storage.new(self)
+    def test_executable_file(self):
+        '''test if file has permissions u+x to execute'''
+        # Check for read access
+        is_read_true = os.access('models/base_model.py', os.R_OK)
+        self.assertTrue(is_read_true)
+        # Check for write access
+        is_write_true = os.access('models/base_model.py', os.W_OK)
+        self.assertTrue(is_write_true)
+        # Check for execution access
+        is_exec_true = os.access('models/base_model.py', os.X_OK)
+        self.assertTrue(is_exec_true)
 
-    def save(self):
-        """ save method """
-        self.updated_at = datetime.now()
-        models.storage.save()
+    def test_is_an_instance(self):
+        '''check if my_model is an instance of BaseModel'''
+        my_model = BaseModel()
+        self.assertIsInstance(my_model, BaseModel)
 
-    def to_dict(self):
-        """ to_dict method """
-        dic = dict(self.__dict__)
-        dic['__class__'] = self.__class__.__name__
-        dic['created_at'] = self.created_at.strftime('%Y-%m-%dT%H:%M:%S.%f')
-        dic['updated_at'] = self.updated_at.strftime('%Y-%m-%dT%H:%M:%S.%f')
-        return dic
+    def test_id(self):
+        '''test if the id of two instances are different'''
+        my_model = BaseModel()
+        my_model1 = BaseModel()
+        self.assertNotEqual(my_model.id, my_model1.id)
 
-    def __str__(self):
-        """ __str__ method """
-        msg = "[{}] ({}) {}"
-        return msg.format(self.__class__.__name__, self.id, self.__dict__)
+    def test_save(self):
+        '''check if the attribute updated_at (date) is updated for
+        the same object with the current date'''
+        my_model2 = BaseModel()
+        first_updated = my_model2.updated_at
+        my_model2.save()
+        second_updated = my_model2.updated_at
+        self.assertNotEqual(first_updated, second_updated)
+
+    def test_to_dict(self):
+        '''check if to_dict returns a dictionary, if add a class
+        key with class name of the object and if updated_at and
+        created_at are converted to string object in ISO format.'''
+        my_model3 = BaseModel()
+        my_dict_model3 = my_model3.to_dict()
+        self.assertIsInstance(my_dict_model3, dict)
+        for key, value in my_dict_model3.items():
+            flag = 0
+            if my_dict_model3['__class__'] == 'BaseModel':
+                flag += 1
+            self.assertTrue(flag == 1)
+        for key, value in my_dict_model3.items():
+            if key == 'created_at':
+                self.assertIsInstance(value, str)
+            if key == 'updated_at':
+                self.assertIsInstance(value, str)
