002b020d8c9125b0d01a1750c3eef0d75a7a0f6b
ayele-getu <102233285+ayele-getu@users.noreply.github.com>
2022-09-05 15:30:46 +0300

    Update file_storage.py

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -23,16 +23,13 @@ class FileStorage:
     def save(self):
         """ Serializes __objects to the JSON file """
         dictionary = {}
-
         for key, value in FileStorage.__objects.items():
             dictionary[key] = value.to_dict()
-
         with open(FileStorage.__file_path, 'w') as f:
             json.dump(dictionary, f)
 
     def reload(self):
-        """ Deserializes __objects from the JSON file """
-        
+        """ Deserializes __objects from the JSON file """        
         from models.base_model import BaseModel
         from models.user import User
         from models.place import Place
@@ -43,7 +40,6 @@ class FileStorage:
         dct = {'BaseModel': BaseModel, 'User': User, 'Place': Place,
                'City': City, 'Amenity': Amenity, 'State': State,
                'Review': Review}
-
         if os.path.exists(FileStorage.__file_path) is True:
             with open(FileStorage.__file_path, 'r') as f:
                 for key, value in json.load(f).items():
