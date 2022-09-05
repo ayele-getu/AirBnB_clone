0c3a768112ff9b4f512cbd01d1f64fe692aae3e0
ayele-getu <102233285+ayele-getu@users.noreply.github.com>
2022-09-05 11:40:26 +0300

    Update test_base_model.py

--- tests/test_models/test_base_model.py
+++ tests/test_models/test_base_model.py
@@ -26,25 +26,32 @@ class BaseModelTests(unittest.TestCase):
         self.assertEqual('BaseModel', my_model_json['__class__'])
         self.assertEqual(self.my_model.id, my_model_json['id'])
 
-    def testSave(self):
-        """ Checks if save method updates the public instance instance
-        attribute updated_at """
-        self.my_model.first_name = "First"
-        self.my_model.save()
+    def test_save(self):
+        '''check if the attribute updated_at (date) is updated for
+        the same object with the current date'''
+        my_model2 = BaseModel()
+        first_updated = my_model2.updated_at
+        my_model2.save()
+        second_updated = my_model2.updated_at
+        self.assertNotEqual(first_updated, second_updated)
 
-        self.assertIsInstance(self.my_model.id, str)
-        self.assertIsInstance(self.my_model.created_at, datetime.datetime)
-        self.assertIsInstance(self.my_model.updated_at, datetime.datetime)
-
-        first_dict = self.my_model.to_dict()
-
-        self.my_model.first_name = "Second"
-        self.my_model.save()
-        sec_dict = self.my_model.to_dict()
-
-        self.assertEqual(first_dict['created_at'], sec_dict['created_at'])
-        self.assertNotEqual(first_dict['updated_at'], sec_dict['updated_at'])
-
+    def test_to_dict(self):
+        '''check if to_dict returns a dictionary, if add a class
+        key with class name of the object and if updated_at and
+        created_at are converted to string object in ISO format.'''
+        my_model3 = BaseModel()
+        my_dict_model3 = my_model3.to_dict()
+        self.assertIsInstance(my_dict_model3, dict)
+        for key, value in my_dict_model3.items():
+            flag = 0
+            if my_dict_model3['__class__'] == 'BaseModel':
+                flag += 1
+            self.assertTrue(flag == 1)
+        for key, value in my_dict_model3.items():
+            if key == 'created_at':
+                self.assertIsInstance(value, str)
+            if key == 'updated_at':
+                self.assertIsInstance(value, str)
 
 if __name__ == '__main__':
     unittest.main()
