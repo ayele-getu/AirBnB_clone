3f09fb5991c608598efe577648fa4a4f36af3e25
ayele-getu <102233285+ayele-getu@users.noreply.github.com>
2022-09-05 15:44:23 +0300

    Update file_storage.py

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -11,38 +11,41 @@ from models.review import Review
 
 
 class FileStorage:
-    """Represent an abstracted storage engine.
-    Attributes:
-        __file_path (str): The name of the file to save objects to.
-        __objects (dict): A dictionary of instantiated objects.
-    """
+    """ Class FileStorage """
     __file_path = "file.json"
     __objects = {}
 
     def all(self):
-        """Return the dictionary __objects."""
-        return FileStorage.__objects
+        """ Return Objects """
+        return (FileStorage.__objects)
 
     def new(self, obj):
-        """Set in __objects obj with key <obj_class_name>.id"""
-        ocname = obj.__class__.__name__
-        FileStorage.__objects["{}.{}".format(ocname, obj.id)] = obj
+        """ Create New Object """
+        FileStorage.__objects["{}.{}".format(
+            obj.__class__.__name__, obj.id)] = obj
 
     def save(self):
-        """Serialize __objects to the JSON file __file_path."""
-        odict = FileStorage.__objects
-        objdict = {obj: odict[obj].to_dict() for obj in odict.keys()}
-        with open(FileStorage.__file_path, "w") as f:
-            json.dump(objdict, f)
+        """ Save File Json """
+        with open(FileStorage.__file_path, 'w') as file_json:
+            res = {}
+            for key, value in FileStorage.__objects.items():
+                res[key] = value.to_dict()
+            file_json.write(json.dumps(res))
 
     def reload(self):
-        """Deserialize the JSON file __file_path to __objects, if it exists."""
-        try:
-            with open(FileStorage.__file_path) as f:
-                objdict = json.load(f)
-                for o in objdict.values():
-                    cls_name = o["__class__"]
-                    del o["__class__"]
-                    self.new(eval(cls_name)(**o))
-        except FileNotFoundError:
-            return
+        """ Reload File Json """
+        try:    
+            with open(FileStorage.__file_path, 'r', encoding="utf-8") as file_json:
+                from models.base_model import BaseModel
+                from models.user import User
+                from models.place import Place
+                from models.state import State
+                from models.city import City
+                from models.amenity import Amenity
+                from models.review import Review
+                json_des = json.load(file_json)
+            for key, value in json_des.items():
+                value = eval(value["__class__"])(**value)
+                FileStorage.__objects[key] = value
+        except OSError:
+            pass
