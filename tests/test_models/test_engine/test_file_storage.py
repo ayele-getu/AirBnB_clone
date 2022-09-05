1716560bbc10fd98bf79e9027556d5e5488ccff7
ayele-getu <102233285+ayele-getu@users.noreply.github.com>
2022-09-05 17:14:49 +0300

    Update test_file_storage.py

--- tests/test_models/test_engine/test_file_storage.py
+++ tests/test_models/test_engine/test_file_storage.py
@@ -1,156 +1,36 @@
 #!/usr/bin/python3
-"""test for file storage"""
-import unittest
-import pep8
-import json
-import os
-from models.base_model import BaseModel
-from models.user import User
-from models.state import State
-from models.city import City
-from models.amenity import Amenity
-from models.place import Place
-from models.review import Review
+'''Tests for FileStorage class'''
+import models
 from models.engine.file_storage import FileStorage
+import os
+import unittest
 
 
-class TestFileStorage(unittest.TestCase):
-    '''this will test the FileStorage'''
-
-    @classmethod
-    def setUpClass(cls):
-        """set up for test"""
-        cls.user = User()
-        cls.user.first_name = "Kev"
-        cls.user.last_name = "Yo"
-        cls.user.email = "1234@yahoo.com"
-        cls.storage = FileStorage()
-        cls.path = "file.json"
-
-    @classmethod
-    def teardown(cls):
-        """at the end of the test this will tear it down"""
-        del cls.user
-        """ if delete the file """
-        if os.path.exists("file.json"):
-            os.remove("file.json")
-
-    def tearDown(self):
-        """teardown"""
-        try:
-            os.remove("file.json")
-        except Exception:
-            pass
-
-    def test_pep8_FileStorage(self):
-        """Tests pep8 style"""
-        style = pep8.StyleGuide(quiet=True)
-        p = style.check_files(['models/engine/file_storage.py'])
-        self.assertEqual(p.total_errors, 0, "fix pep8")
+class TestFile_Storage(unittest.TestCase):
+    '''start tests'''
 
     def test_docstring(self):
-        """
-        Test docstring
-        """
-        self.assertIsNotNone(FileStorage.__doc__)
+        '''test if funcions, methods, classes
+        and modules have docstring'''
+        msj = "Module doesnt have docstring"
+        obj = models.engine.file_storage.__doc__
+        self.assertIsNotNone(obj, msj)  # Modules
+        msj = "Classes doesnt have docstring"
+        self.assertIsNotNone(obj, msj)  # Classes
 
-    def test_documentation(self):
-        """
-        Test documentation, created and not empty
-        """
-        self.assertTrue(FileStorage.all.__doc__)
-        self.assertIsNotNone(FileStorage.all.__doc__)
-        self.assertTrue(FileStorage.new.__doc__)
-        self.assertIsNotNone(FileStorage.new.__doc__)
-        self.assertTrue(FileStorage.save.__doc__)
-        self.assertIsNotNone(FileStorage.save.__doc__)
-        self.assertTrue(FileStorage.reload.__doc__)
-        self.assertIsNotNone(FileStorage.reload.__doc__)
+    def test_executable_file(self):
+        '''test if file has permissions u+x to execute'''
+        # Check for read access
+        is_read_true = os.access('models/engine/file_storage.py', os.R_OK)
+        self.assertTrue(is_read_true)
+        # Check for write access
+        is_write_true = os.access('models/engine/file_storage.py', os.W_OK)
+        self.assertTrue(is_write_true)
+        # Check for execution access
+        is_exec_true = os.access('models/engine/file_storage.py', os.X_OK)
+        self.assertTrue(is_exec_true)
 
-    def test_all(self):
-        """tests if all works in File Storage"""
-        storage = FileStorage()
-        obj = storage.all()
-        self.assertIsNotNone(obj)
-        self.assertEqual(type(obj), dict)
-        self.assertIs(obj, storage._FileStorage__objects)
-
-    def test_new(self):
-        """test when new is created"""
-        storage = FileStorage()
-        obj = storage.all()
-        user = User()
-        user.id = "123455"
-        user.name = "Kevin"
-        storage.new(user)
-        key = user.__class__.__name__ + "." + str(user.id)
-        self.assertIsNotNone(obj[key])
-
-    def test_reload_filestorage(self):
-        """
-        tests reload
-        """
-        self.storage.save()
-        Root = os.path.dirname(os.path.abspath("console.py"))
-        path = os.path.join(Root, "file.json")
-        with open(path, 'r') as f:
-            lines = f.readlines()
-        try:
-            os.remove(path)
-        except Exception:
-            pass
-        self.storage.save()
-        with open(path, 'r') as f:
-            lines2 = f.readlines()
-        self.assertEqual(lines, lines2)
-        try:
-            os.remove(path)
-        except Exception:
-            pass
-        with open(path, "w") as f:
-            f.write("{}")
-        with open(path, "r") as r:
-            for line in r:
-                self.assertEqual(line, "{}")
-        self.assertIs(self.storage.reload(), None)
-
-    def test_file_path(self):
-        """
-        Test to see if the file_self.path exist
-        """
-        try:
-            self.assertEqual(FileStorage._FileStorage__file_path, self.path)
-        except AttributeError:
-            pass
-
-    def test_objects_exist_storage(self):
-        """
-        Test if __objects exist and was created
-        """
-        dic = self.storage.all()
-        try:
-            self.assertEqual(FileStorage._FileStorage__objects, dic)
-            self.assertTrue(FileStorage._FileStorage__objects)
-        except AttributeError:
-            pass
-
-    def test_save(self):
-        """
-        Testing the save method
-        """
-        bm = BaseModel()
-        bm.save()
-        self.assertTrue(os.path.exists(self.path))
-        bm.name = "Testing"
-        bm.number = 1
-        bm.save()
-        self.assertTrue(os.path.exists(self.path))
-        dic = {}
-        with open('file.json', 'r') as fjson:
-            dic = json.loads(fjson.read())
-        bm_key = bm.__class__.__name__ + '.' + bm.id
-        self.assertDictEqual(bm.to_dict(), dic[bm_key])
-
-
-if __name__ == "__main__":
-    unittest.main()
+    def test_is_an_instance(self):
+        '''check if my_model is an instance of BaseModel'''
+        my_model = FileStorage()
+        self.assertIsInstance(my_model, FileStorage)
