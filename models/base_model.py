57fd6d5ca6728ff426be5c6d53036f7c9e9deff1
ayele-getu <102233285+ayele-getu@users.noreply.github.com>
2022-09-05 17:41:09 +0300

    Update base_model.py

--- models/base_model.py
+++ models/base_model.py
@@ -1,44 +1,48 @@
 #!/usr/bin/python3
+"""Module for Base class
+Contains the Base class for the AirBnB clone console.
+"""
 
-""" import modules for set id and datetime """
 import uuid
 from datetime import datetime
 import models
 
 
-class BaseModel():
-    """ BaseModel Class """
+class BaseModel:
 
+    """Class for base model"""
     def __init__(self, *args, **kwargs):
-
-        if bool(kwargs):
-            for name_attr, value in kwargs.items():
-                if name_attr in ["created_at", "updated_at"]:
-                    setattr(self,
-                            name_attr,
-                            datetime.strptime(value, '%Y-%m-%dT%H:%M:%S.%f'))
-                elif name_attr != "__class__":
-                    setattr(self, name_attr, value)
+        """Initialization"""
+        if kwargs is not None and kwargs != {}:
+            for key in kwargs:
+                if key == "created_at":
+                    self.__dict__["created_at"] = datetime.strptime(
+                        kwargs["created_at"], "%Y-%m-%dT%H:%M:%S.%f")
+                elif key == "updated_at":
+                    self.__dict__["updated_at"] = datetime.strptime(
+                        kwargs["updated_at"], "%Y-%m-%dT%H:%M:%S.%f")
+                else:
+                    self.__dict__[key] = kwargs[key]
         else:
             self.id = str(uuid.uuid4())
             self.created_at = datetime.now()
-            self.updated_at = self.created_at
+            self.updated_at = datetime.now()
             models.storage.new(self)
 
+    def __str__(self):
+        """str format"""
+        return "[{}] ({}) {}".\
+            format(type(self).__name__, self.id, self.to_dict())
+
     def save(self):
-        """ save method """
+        """updates."""
         self.updated_at = datetime.now()
         models.storage.save()
 
     def to_dict(self):
-        """ to_dict method """
-        dic = dict(self.__dict__)
-        dic['__class__'] = self.__class__.__name__
-        dic['created_at'] = self.created_at.strftime('%Y-%m-%dT%H:%M:%S.%f')
-        dic['updated_at'] = self.updated_at.strftime('%Y-%m-%dT%H:%M:%S.%f')
-        return dic
-
-    def __str__(self):
-        """ __str__ method """
-        msg = "[{}] ({}) {}"
-        return msg.format(self.__class__.__name__, self.id, self.__dict__)
+        """dictionary format"""
+        new_dict = dict(self.__dict__)
+        new_dict["__class__"] = type(self).__name__
+        new_dict["created_at"] = new_dict["created_at"].isoformat()
+        new_dict["updated_at"] = new_dict["updated_at"].isoformat()
+        return new_dict
