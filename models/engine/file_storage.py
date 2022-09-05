8915fa703334a51fa7ee732f28a614411cac2207
ayele-getu <102233285+ayele-getu@users.noreply.github.com>
2022-09-05 15:49:04 +0300

    Update file_storage.py

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -1,13 +1,6 @@
 #!/usr/bin/python3
-"""Defines the FileStorage class."""
 import json
-from models.base_model import BaseModel
-from models.user import User
-from models.state import State
-from models.city import City
-from models.place import Place
-from models.amenity import Amenity
-from models.review import Review
+import os
 
 
 class FileStorage:
